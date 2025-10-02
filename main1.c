afc38cfce3df8481d20c7e3b9babd8f14fe131c0
B1429053 <fionzhang415@gmail.com>
2025-10-02 13:54:27 +0800

    Update main1.c

--- main1.c
+++ main1.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main() {
-  int n = 6;
+    int n = 6;
   
-  for(int i=1;i<=n;i++){
-for(int j=1;j<=n-i;j++){
-printf(" ");
-}
-    for(int k=1;k<=i;k++){
-      printf("%d ",i);
-    }  
-printf("\n");
+    for(int i=1;i<=n;i++){
+       for(int j=1;j<=n-i;j++){
+          printf(" ");
        }
+       for(int k=1;k<=i;k++){
+          printf("%d ",i);
+       }  
+       printf("\n");
+   }
   
-  return 0;
+   return 0;
 }
